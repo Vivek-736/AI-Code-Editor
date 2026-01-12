ffe3319709b21a570beb23410dffdd3f3f1b5649
Vivek <vivekousik8555@gmail.com>
2026-01-12 23:10:33 +0530

    Initialize the convex database setup with providers wrapped around the layout.tsx file with all the providers unified in one

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,26 @@
+"use client";
+
+import { useMutation, useQuery } from "convex/react";
+import { api } from "../../convex/_generated/api";
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
+  const projects = useQuery(api.projects.get);
+  const createProject = useMutation(api.projects.create);
+
   return (
-    <div className="text-xl p-2">
-      Orbit
+    <div className="flex flex-col gap-2 p-4">
+      <Button onClick={() => createProject({
+        name: "New Project"
+      })}>
+        Create Project
+      </Button>
+      {projects?.map((project) => (
+        <div key={project._id} className="border-2 rounded p-2 flex flex-col">
+          <p>{project.name}</p>
+          <p>Owner ID: {project.ownerId}</p>
+        </div>
+      ))}
     </div>
   );
 }
