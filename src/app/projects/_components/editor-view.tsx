67e8d9b4b95a6ac53b1a6610c2c242255c2ee4c3
Vivek <vivekousik8555@gmail.com>
2026-01-18 17:57:53 +0530

    Added the activeTabId to the editor view from the useEditor hook

--- src/app/projects/_components/editor-view.tsx
+++ src/app/projects/_components/editor-view.tsx
@@ -1,7 +1,10 @@
+import { useEditor } from "@/hooks/use-editor";
 import { Id } from "../../../../convex/_generated/dataModel";
 import TopNavigation from "./top-navigation";
 
 const EditorView = ({ projectId }: { projectId: Id<"projects"> }) => {
+    const { activeTabId } = useEditor(projectId);
+
     return (
         <div className="h-full flex flex-col">
             <div className="flex items-center">
